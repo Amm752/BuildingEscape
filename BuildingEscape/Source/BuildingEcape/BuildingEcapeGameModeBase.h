67b54606a8d088af2552f0eec96cb57f9d263736
Ahmad Meer <Amm752@nau.edu>
2017-09-06 22:43:52 -0700

    Inital Commit

---
+++ BuildingEscape/Source/BuildingEcape/BuildingEcapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEcapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGECAPE_API ABuildingEcapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
